c7293e9e5f23b6a02f1dbf4839d95c1991d1e90f
Walker Christie <walker781@yahoo.com>
2017-07-29 18:31:41 -0400

    Splats now blend between textures

--- Assets/Custom/Scripts/Texturing/TerrainPaint.cs
+++ Assets/Custom/Scripts/Texturing/TerrainPaint.cs
@@ -40,7 +40,7 @@ public class TerrainPaint {
 		Angle,
 	}
 
-	public int AlphaMapResolution = 128 * 2; //128
+	public int AlphaMapResolution = 128; //128
 
 	GameObject TerrainObject;
 	Material TerrainMaterial;
@@ -48,9 +48,6 @@ public class TerrainPaint {
 	Vector3[] Vertices;
 	Vector3[] Normals;
 	int MeshResolution;
-
-	float MaxHeight = float.NegativeInfinity;
-	float MinHeight = float.PositiveInfinity;
 
 	/// <summary>
 	/// Create a TerrainPaint object that paints the passed gameobject
@@ -85,14 +82,12 @@ public class TerrainPaint {
 					settings.SplatSettings.RemoveAt(i);
 					i--;
 					continue;
-				}
-
-				EditorGUILayout.BeginVertical();
-				splat.MainTexture = (Texture)EditorGUILayout.ObjectField("Main Texture", splat.MainTexture, typeof(Texture), true, GUILayout.Height(16));
-				splat.NormalTexture = (Texture)EditorGUILayout.ObjectField("Normal Texture", splat.NormalTexture, typeof(Texture), true, GUILayout.Height(16));
-				EditorGUILayout.EndVertical();
+				}				
+				EditorGUILayout.LabelField((splat.PlacementType == PlacementType.Angle ? "Angled" : "Elevation") + " Texture " + (i + 1));
 				EditorGUILayout.EndHorizontal();
 
+				splat.MainTexture = (Texture) EditorGUILayout.ObjectField("Main Texture", splat.MainTexture, typeof(Texture), true, GUILayout.Height(16));
+				splat.NormalTexture = (Texture) EditorGUILayout.ObjectField("Normal Texture", splat.NormalTexture, typeof(Texture), true, GUILayout.Height(16));
 				splat.Metallic = EditorGUILayout.Slider("Metallic", splat.Metallic, 0f, 1f);
 				splat.Smoothness = EditorGUILayout.Slider("Smoothness", splat.Smoothness, 0f, 1f);
 				splat.Tiling = EditorGUILayout.Vector2Field("Tiling", splat.Tiling);
@@ -136,7 +131,7 @@ public class TerrainPaint {
 				float height = sample.Height;
 				float angle = sample.Angle;
 				float[] weights = new float[settings.Count];
-				var blend = 20f; //TODO: Actually put in settings
+				var blend = 10f; //TODO: Actually put in settings
 
 				for (int i = 0; i < settings.Count; i++) {
 					SplatSetting splat = settings[i];
@@ -147,8 +142,16 @@ public class TerrainPaint {
 								weights[i] = 0f; //TODO: Fix
 							break;
 						case PlacementType.ElevationRange: 
-							if (height > splat.MinRange && height < splat.MaxRange)
-								weights[i] = Mathf.Clamp01((blend - (splat.MinRange - height)) / blend);
+							if (height > splat.MinRange && height < splat.MaxRange) {
+								if (i > 0) { //Can blend up
+									float factor = Mathf.Clamp01((blend - (height - splat.MinRange)) / blend);
+									weights[i - 1] = factor;
+									weights[i] = 1 - factor;
+								} else {
+									weights[i] = 1f;
+								}
+							}
+
 							break;
 					}
 				}
@@ -182,122 +185,6 @@ public class TerrainPaint {
 			byte[] bytes = Tex.EncodeToPNG();
 			File.WriteAllBytes(Application.dataPath + "/Splat.png", bytes);
 		}
-	}
-
-	//public void X() {
-
-	//	terrainData.splatPrototypes = Textures.Select(s => new SplatPrototype { texture = s.Texture }).ToArray();                   //Get all the textures and assign it to the terrain's spaltprototypes
-	//	terrainData.RefreshPrototypes();                                                                                            //gotta refresh my terraindata's prototypes after its manipulated
-
-	//	int splatLengths = terrainData.splatPrototypes.Length;
-	//	int alphaMapResolution = terrainData.alphamapResolution;
-	//	int alphaMapHeight = terrainData.alphamapResolution;
-	//	int alphaMapWidth = terrainData.alphamapResolution;
-
-	//	var splatMap = new float[alphaMapResolution, alphaMapResolution, splatLengths];       //create a new splatmap array equal to our map's, we will store our new splat weights in here, then assight it to the map later
-	//	var heights = terrainData.GetHeights(0, 0, alphaMapWidth, alphaMapHeight);                                 //get all the height points for the terrain... this will be where ware are going paint our textures on
-
-	//	for (var zRes = 0; zRes < alphaMapHeight; zRes++) {
-	//		for (var xRes = 0; xRes < alphaMapWidth; xRes++) {
-	//			var splatWeights = new float[splatLengths];                                             //create a temp array to store all our 'none-normalised weights'
-	//			var normalizedX = (float)xRes / (alphaMapWidth - 1);                        //gets the normalised X position based on the map resolution                     
-	//			var normalizedZ = (float)zRes / (alphaMapHeight - 1);                       //gets the normalised Y position based on the map resolution 
-	//			var randomBlendNoise = ReMap(Mathf.PerlinNoise(xRes * .8f, zRes * .5f), 0, 1, .8f, 1);  //Get a random perlin value
-
-	//			float angle = terrainData.GetSteepness(normalizedX, normalizedZ);                       //Get the ANGLE/STEEPNESS at this point: returns the angle between 0 and 90
-	//			Vector3 direction = terrainData.GetInterpolatedNormal(xRes, zRes);                      //Get the DIRECTION at this point: returns the direction of the normal as a Vector3
-	//			float elevation = heights[zRes, xRes];                                                  //Get the HEIGHT at this point: return between 0 and 1 (0=lowest trough, .5f=Water level. 1f=highest peak)
-	//			float perlinElevation = heights[zRes, xRes] * randomBlendNoise;                         //Get a semi random height based on perlin noise, this is to give a more random blend, rather than straight horizontal lines.
-
-	//			for (var i = 0; i < Textures.Length; i++)                                               //Loop through all our trextures and apply them accoding to the rules defined
-	//			{
-	//				var weighting = 0f;                                                                 //set the default weighting to 0, this means that if the image does not meet any of the criteria, then it will have no impact
-	//				var textureSetting = Textures[i];                                                   //get the setting instance based on index
-	//				var calculatedHeight = textureSetting.RandomBlend ? perlinElevation : elevation;    //create a new height variable, and make it the actual height, unless the user selected to add a bit of randomness                                      
-
-	//				switch (textureSetting.PlacementType) {
-	//					case PlacementType.Angle:
-	//						if (Math.Abs(angle - textureSetting.Angle) < textureSetting.Precisision)                        //check if the specified angle is the same as the current angle (allow a variance based on the precision)
-	//							weighting = textureSetting.Impact;
-	//						break;
-	//					case PlacementType.Direction:
-	//						if (Vector3.SqrMagnitude(direction - textureSetting.Direction) < textureSetting.Precisision)    //check if the specified direction is the same as the current direction (allow a variance based on the precision)
-	//							weighting = textureSetting.Impact;
-	//						break;
-	//					case PlacementType.Elevation:
-	//						if (Math.Abs(textureSetting.Elevation = calculatedHeight) < textureSetting.Precisision)         //check if the specified elevation is the same as the current elevation (allow a variance based on the precision)
-	//							weighting = textureSetting.Impact;
-	//						break;
-	//					case PlacementType.ElevationRange:
-	//						if (calculatedHeight > textureSetting.MinRange && calculatedHeight < textureSetting.MaxRange)    //check if the current height is between the specified min and max heights
-	//							weighting = textureSetting.Impact;
-	//						break;
-	//				}
-
-	//				splatWeights[i] = weighting;
-	//			}
-
-	//			#region normalize
-	//			//we need to make sure that the sum of our weights is not greater than 1, so lets normalise it
-	//			var totalWeight = splatWeights.Sum();                               //sum all the splat weights,
-	//			for (int i = 0; i < splatLengths; i++)        //Loop through each splatWeights
-	//			{
-	//				splatWeights[i] /= totalWeight;                                 //Normalize so that sum of all texture weights = 1
-	//				splatMap[zRes, xRes, i] = splatWeights[i];                      //Assign this point to the splatmap array
-	//			}
-	//			#endregion
-	//		}
-	//	}
-
-	//	terrainData.SetAlphamaps(0, 0, splatMap);
-	//}
-
-	/// <summary>
-	/// Gets the highest vertex point y on the mesh and caches result.
-	/// </summary>
-	/// <returns>Highest vertex height</returns>
-	float GetMaxHeight() {
-		if (float.IsNegativeInfinity(MaxHeight)) {
-			float y = float.NegativeInfinity;
-
-			foreach (Vector3 pos in Mesh.vertices) {
-				if (pos.y > y)
-					y = pos.y;
-			}
-
-			MaxHeight = y;
-		}
-
-		return MaxHeight;
-	}
-
-	/// <summary>
-	/// Gets the lowest vertex point y on the mesh and caches result.
-	/// </summary>
-	/// <returns>Lowest vertex height</returns>
-	float GetMinHeight() {
-		if (float.IsInfinity(MinHeight)) {
-			float y = float.PositiveInfinity;
-
-			foreach (Vector3 pos in Mesh.vertices) {
-				if (pos.y < y)
-					y = pos.y;
-			}
-
-			MinHeight = y;
-		}
-
-		return MinHeight;
-	}
-
-	/// <summary>
-	/// Resets both min and max height samples to their 
-	/// respective defaults allowing GetMinHeight and 
-	/// GetMaxHeight to be recalculated.
-	/// </summary>
-	void ResetCachedHeightSamples() {
-		MaxHeight = float.NegativeInfinity;
-		MinHeight = float.PositiveInfinity;
 	}
 
 	/// <summary>

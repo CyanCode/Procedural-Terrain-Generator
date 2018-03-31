df23a26222ab73826058b4fe77774403b3685090
Walker Christie <walker781@yahoo.com>
2018-03-31 17:43:40 -0400

    Test build / editor differences using test

---
+++ Assets/Tests/TerraStartTest.cs
@@ -0,0 +1,27 @@
+﻿using Terra.Terrain;
+using UnityEngine;
+
+public class TerraStartTest : MonoBehaviour {
+	public TerraSettings Settings;
+	public GameObject FPSController;
+
+	bool Started = false;
+	Vector3 InitPos;
+
+	void Start() {
+		InitPos = FPSController.transform.position;
+	}
+
+	void Update() {
+		if (!Started) {
+			FPSController.transform.position = InitPos;
+		}
+
+		if (Input.GetMouseButtonDown(0)) {
+			Started = true;
+
+			if (!Settings.GenerateOnStart)
+				Settings.Generate();
+		}
+	}
+}

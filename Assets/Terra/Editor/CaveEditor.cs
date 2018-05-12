8f08bb265abfaf50eb803cf5214aa9c1198b0e83
Walker Christie <walker781@yahoo.com>
2018-05-12 14:51:59 -0400

    Start CaveEditor setup

---
+++ Assets/Terra/Editor/CaveEditor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Terra.Terrain.Cave {
+	[CustomEditor(typeof(CaveGenerator))]
+	public class CaveEditor: Editor {
+		public override void OnInspectorGUI() {
+			DrawDefaultInspector();
+
+			if (GUILayout.Button("Update Cave")) {
+				
+			}
+		}
+	}
+}

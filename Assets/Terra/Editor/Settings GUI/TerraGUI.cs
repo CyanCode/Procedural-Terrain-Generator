0edc99c446ed6e2c2ffb4847d0c6331b741562c1
Walker Christie <walker781@yahoo.com>
2018-06-01 00:57:18 -0400

    Updated look and functionality of material editor. Included reorderable array

--- Assets/Terra/Editor/Settings GUI/TerraGUI.cs
+++ Assets/Terra/Editor/Settings GUI/TerraGUI.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Terra.Graph.Noise;
+using Terra.ReorderableList;
 using Terra.Terrain;
 using Terra.Terrain.Util;
 using UnityEngine;
@@ -13,8 +14,11 @@ namespace UnityEditor.Terra {
 		private TerraSettings Settings;
 		private Texture[] ToolbarImages;
 
+		private ReorderableMaterialList matList;
+
 		public TerraGUI(TerraSettings settings) {
 			this.Settings = settings;
+			matList = new ReorderableMaterialList(settings);
 		}
 
 		/// <summary>
@@ -101,102 +105,104 @@ namespace UnityEditor.Terra {
 
 			//Use textures
 			if (Settings.SplatSettings != null) {
+				ReorderableListGUI.ListField(matList);
+
 				for (int i = 0; i < Settings.SplatSettings.Count; i++) {
-					TerrainPaint.SplatSetting splat = Settings.SplatSettings[i];
+					//	TerrainPaint.SplatSetting splat = Settings.SplatSettings[i];
 
-					//Surround each material w/ box
-					GUIStyle boxStyle = new GUIStyle();
-					boxStyle.padding = new RectOffset(3, 3, 3, 3);
-					boxStyle.normal.background = GetWhiteTexture();
-					EditorGUILayout.BeginVertical(boxStyle);
+					//	//Surround each material w/ box
+					//	GUIStyle boxStyle = new GUIStyle();
+					//	boxStyle.padding = new RectOffset(3, 3, 3, 3);
+					//	boxStyle.normal.background = GetWhiteTexture();
+					//	EditorGUILayout.BeginVertical(boxStyle);
 
-					//Close button / name
-					EditorGUILayout.BeginHorizontal();
-					if (GUILayout.Button("X", GUILayout.Height(16), GUILayout.Width(18))) {
-						Settings.SplatSettings.RemoveAt(i);
-						i--;
-						continue;
-					}
-					//EditorGUILayout.LabelField((splat.PlacementType == PlacementType.Angle ? "Angled" : "Elevation") + " Material " + (i + 1));
-					EditorGUILayout.LabelField("Elevation Material " + (i + 1));
-					EditorGUILayout.EndHorizontal();
+					//	//Close button / name
+					//	EditorGUILayout.BeginHorizontal();
+					//	if (GUILayout.Button("X", GUILayout.Height(16), GUILayout.Width(18))) {
+					//		Settings.SplatSettings.RemoveAt(i);
+					//		i--;
+					//		continue;
+					//	}
+					//	//EditorGUILayout.LabelField((splat.PlacementType == PlacementType.Angle ? "Angled" : "Elevation") + " Material " + (i + 1));
+					//	EditorGUILayout.LabelField("Elevation Material " + (i + 1));
+					//	EditorGUILayout.EndHorizontal();
 
-					//Material settings
-					EditorGUILayout.Space();
+					//	//Material settings
+					//	EditorGUILayout.Space();
 
-					if (splat.Diffuse == null) {
-						EditorGUILayout.HelpBox("This splat material does not have a selected diffuse texture.", MessageType.Warning);
-					} else {
-						EditorGUILayout.BeginHorizontal();
-						EditorGUILayout.BeginVertical();
+					//	if (splat.Diffuse == null) {
+					//		EditorGUILayout.HelpBox("This splat material does not have a selected diffuse texture.", MessageType.Warning);
+					//	} else {
+					//		EditorGUILayout.BeginHorizontal();
+					//		EditorGUILayout.BeginVertical();
 
-						splat.Diffuse = (Texture2D)EditorGUILayout.ObjectField(splat.Diffuse,
-							typeof(Texture2D), false, GUILayout.Width(80), GUILayout.Height(80));
-						EditorGUILayout.LabelField("Diffuse", GUILayout.Width(60));
-						EditorGUILayout.EndVertical();
+					//		splat.Diffuse = (Texture2D)EditorGUILayout.ObjectField(splat.Diffuse,
+					//			typeof(Texture2D), false, GUILayout.Width(80), GUILayout.Height(80));
+					//		EditorGUILayout.LabelField("Diffuse", GUILayout.Width(60));
+					//		EditorGUILayout.EndVertical();
 
-						if (splat.Normal != null) {
-							EditorGUILayout.BeginVertical();
-							splat.Normal = (Texture2D)EditorGUILayout.ObjectField(splat.Normal,
-								typeof(Texture2D), false, GUILayout.Width(80), GUILayout.Height(80));
-							EditorGUILayout.LabelField("Normal", GUILayout.Width(60));
-							EditorGUILayout.EndVertical();
-						}
+					//		if (splat.Normal != null) {
+					//			EditorGUILayout.BeginVertical();
+					//			splat.Normal = (Texture2D)EditorGUILayout.ObjectField(splat.Normal,
+					//				typeof(Texture2D), false, GUILayout.Width(80), GUILayout.Height(80));
+					//			EditorGUILayout.LabelField("Normal", GUILayout.Width(60));
+					//			EditorGUILayout.EndVertical();
+					//		}
 
-						EditorGUILayout.EndHorizontal();
-					}
-					if (GUILayout.Button("Edit Material")) {
-						AddTextureWindow.Init(ref splat);
-					}
+					//		EditorGUILayout.EndHorizontal();
+					//	}
+					//	if (GUILayout.Button("Edit Material")) {
+					//		AddTextureWindow.Init(ref splat);
+					//	}
 
-					EditorGUILayout.Space();
+					//	EditorGUILayout.Space();
 
-					//Blend factor
-					splat.Blend = EditorGUILayout.FloatField("Blend Amount", splat.Blend);
+					//	//Blend factor
+					//	splat.Blend = EditorGUILayout.FloatField("Blend Amount", splat.Blend);
 
-					//GUI for different types
-					splat.PlacementType = (TerrainPaint.PlacementType)EditorGUILayout.EnumPopup("Placement Type", splat.PlacementType);
-					switch (splat.PlacementType) {
-						case TerrainPaint.PlacementType.Angle:
-							EditorGUILayout.LabelField("Min Angle", splat.AngleMin.ToString("0") + " deg");
-							EditorGUILayout.LabelField("Max Angle", splat.AngleMax.ToString("0") + " deg");
-							EditorGUILayout.MinMaxSlider(ref splat.AngleMin, ref splat.AngleMax, 0f, 90f);
-							break;
-						case TerrainPaint.PlacementType.ElevationRange:
-							if (!splat.IsMaxHeight)
-								splat.MaxRange = EditorGUILayout.FloatField("Max Height", splat.MaxRange);
-							if (!splat.IsMinHeight)
-								splat.MinRange = EditorGUILayout.FloatField("Min Height", splat.MinRange);
+					//	//GUI for different types
+					//	splat.PlacementType = (TerrainPaint.PlacementType)EditorGUILayout.EnumPopup("Placement Type", splat.PlacementType);
+					//	switch (splat.PlacementType) {
+					//		case TerrainPaint.PlacementType.Angle:
+					//			EditorGUILayout.LabelField("Min Angle", splat.AngleMin.ToString("0") + " deg");
+					//			EditorGUILayout.LabelField("Max Angle", splat.AngleMax.ToString("0") + " deg");
+					//			EditorGUILayout.MinMaxSlider(ref splat.AngleMin, ref splat.AngleMax, 0f, 90f);
+					//			break;
+					//		case TerrainPaint.PlacementType.ElevationRange:
+					//			if (!splat.IsMaxHeight)
+					//				splat.MaxRange = EditorGUILayout.FloatField("Max Height", splat.MaxRange);
+					//			if (!splat.IsMinHeight)
+					//				splat.MinRange = EditorGUILayout.FloatField("Min Height", splat.MinRange);
 
-							//Checkboxes for infinity & -infinity heights
-							EditorGUI.BeginChangeCheck();
-							if (splat.IsMaxHeight || !Settings.IsMaxHeightSelected) splat.IsMaxHeight = EditorGUILayout.Toggle("Is Highest Material", splat.IsMaxHeight);
-							if (EditorGUI.EndChangeCheck())
-								Settings.IsMaxHeightSelected = splat.IsMaxHeight;
+					//			//Checkboxes for infinity & -infinity heights
+					//			EditorGUI.BeginChangeCheck();
+					//			if (splat.IsMaxHeight || !Settings.IsMaxHeightSelected) splat.IsMaxHeight = EditorGUILayout.Toggle("Is Highest Material", splat.IsMaxHeight);
+					//			if (EditorGUI.EndChangeCheck())
+					//				Settings.IsMaxHeightSelected = splat.IsMaxHeight;
 
-							EditorGUI.BeginChangeCheck();
-							if (splat.IsMinHeight || !Settings.IsMinHeightSelected) splat.IsMinHeight = EditorGUILayout.Toggle("Is Lowest Material", splat.IsMinHeight);
-							if (EditorGUI.EndChangeCheck())
-								Settings.IsMinHeightSelected = splat.IsMinHeight;
+					//			EditorGUI.BeginChangeCheck();
+					//			if (splat.IsMinHeight || !Settings.IsMinHeightSelected) splat.IsMinHeight = EditorGUILayout.Toggle("Is Lowest Material", splat.IsMinHeight);
+					//			if (EditorGUI.EndChangeCheck())
+					//				Settings.IsMinHeightSelected = splat.IsMinHeight;
 
-							//if (splat.MinRange > splat.MaxRange) splat.MinRange = splat.MaxRange;
-							break;
-					}
+					//			//if (splat.MinRange > splat.MaxRange) splat.MinRange = splat.MaxRange;
+					//			break;
+					//	}
 
-					EditorGUILayout.EndVertical();
-					EditorGUILayout.Separator();
+					//	EditorGUILayout.EndVertical();
+					//	EditorGUILayout.Separator();
+					//}
 				}
+					if (GUILayout.Button("Add Material")) {
+						if (Settings.SplatSettings == null)
+							Settings.SplatSettings = new List<TerrainPaint.SplatSetting>();
 
-				if (GUILayout.Button("Add Material")) {
-					if (Settings.SplatSettings == null)
-						Settings.SplatSettings = new List<TerrainPaint.SplatSetting>();
+						Settings.SplatSettings.Add(new TerrainPaint.SplatSetting());
+					}
 
-					Settings.SplatSettings.Add(new TerrainPaint.SplatSetting());
-				}
-				
-				if (Settings.DisplayPreview && GUILayout.Button("Update Preview")) {
-					Settings.Preview.TriggerMaterialsUpdate();
-				}
+					if (Settings.DisplayPreview && GUILayout.Button("Update Preview")) {
+						Settings.Preview.TriggerMaterialsUpdate();
+					}
 			}
 		}
 

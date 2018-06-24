98501e7a78e0c8c3682564554b0c8e6aa5fd5046
Walker Christie <walker781@yahoo.com>
2018-06-24 18:22:14 -0400

    Added LOD features

---
+++ Assets/Terra/Source/LODLevel.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using Terra.Terrain;
+using UnityEngine;
+
+/// <summary>
+/// Handles assigning LOD levels to individual TerrainTiles 
+/// and changing the resolution at runtime.
+/// </summary>
+public class LODLevel {
+    private TerrainTile Tile;
+    private List<Level> Levels;
+
+    public LODLevel(TerrainTile tile) {
+        Tile = tile;
+        Levels = new List<Level>();
+    }
+
+    public LODLevel(TerrainTile tile, List<Level> levels) {
+        Tile = tile;
+        Levels = levels;
+    }
+
+    public LODLevel(TerrainTile tile, Level level) : this(tile) {
+        Levels.Add(level);
+    }
+
+    /// <summary>
+    /// Sets the available LOD levels of this TerrainTile
+    /// </summary>
+    /// <param name="levels"></param>
+    public void SetLODLevels(List<Level> levels) {
+        Levels = levels;
+    }
+
+    /// <summary>
+    /// Adds a new LOD level to this TerrainTile
+    /// </summary>
+    /// <param name="level"></param>
+    public void AddLODLevel(Level level) {
+        Levels.Add(level);
+    }
+
+    /// <summary>
+    /// Activates an LOD Level and applies the changes to the 
+    /// assigned TerrainTile. If the vertex map hasn't already been 
+    /// computed for the requested LOD level, it is computed.
+    /// </summary>
+    /// <param name="level">level to activate</param>
+    /// <exception cref="LevelNotSetException">Thrown when the passed level hasn't been set</exception>
+    public void ActivateLODLevel(int level) {
+        Level l = GetLevel(level);
+        if (l == null) {
+            throw new LevelNotSetException();
+        }
+
+        if (!l.HasHeightmap()) {
+            PrecomputeLODLevel(l.LevelNum);
+        }
+
+        var md = Tile.CreateRawMesh(l.VertexMap, l.Resolution);
+        Tile.RenderRawMeshData(md);
+    }
+
+    /// <summary>
+    /// Precomputes the necessary information needed by the 
+    /// passed LOD level to change the resolution of the 
+    /// current TerrainTile. Consider calling this before 
+    /// <see cref="ActivateLODLevel(int)"></see> as computing a heightmap 
+    /// can take time. This method is thread safe.
+    /// </summary>
+    /// <param name="level">level to precompute</param>
+    /// <exception cref="LevelNotSetException">Thrown when the passed level hasn't been set</exception>
+    public void PrecomputeLODLevel(int level) {
+        Level l = GetLevel(level);
+        if (l == null) {
+            throw new LevelNotSetException();
+        }
+
+        //Setup vertex map
+        l.VertexMap = new Vector3[l.Resolution * l.Resolution];
+        for (int x = 0; x < l.Resolution; x++) {
+            for (int z = 0; z < l.Resolution; z++) {
+                l.VertexMap[x + z * l.Resolution] = Tile.GetPositionAt(x, z, l.Resolution);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Finds the LOD level that is internally stored and 
+    /// returns it if its found.
+    /// </summary>
+    /// <param name="level">level to search for</param>
+    /// <returns>Level instance if found, null otherwise</returns>
+    public Level GetLevel(int level) {
+        foreach (Level l in Levels) {
+            if (l.LevelNum == level) {
+                return l;
+            }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Container class for information level of detail data. 
+    /// Contains the resolution of the heightmap and the cached 
+    /// heightmap if one is available.
+    /// </summary>
+    public class Level {
+        public int Resolution = 128;
+
+        /// <summary>
+        /// Array of Vector3 positions in world space that each represent 
+        /// a single vertex. Indicies should be accessed using the following 
+        /// equation: x + z * resolution = position
+        /// </summary>
+        public Vector3[] VertexMap = null;
+
+        public int LevelNum;
+
+        public Level(int resolution, int level) {
+            Resolution = resolution;
+            LevelNum = level;
+        }
+
+        public bool HasHeightmap() {
+            return VertexMap != null; 
+        }
+    }
+
+    /// <summary>
+    /// This exception occurs when a numerical LOD level 
+    /// is passed but this LODLevel instance hasn't been assigned 
+    /// the passed level.
+    /// </summary>
+    public class LevelNotSetException : Exception {
+        public LevelNotSetException(string message) : base(message) { }
+
+        public LevelNotSetException() : base("A LOD level was not set for the passed number") { }
+    }
+}
--- Assets/Terra/Source/TerrainTile.cs
+++ Assets/Terra/Source/TerrainTile.cs
@@ -77,7 +77,7 @@ namespace Terra.Terrain {
 		}
 
 		/// <summary>
-		/// Polls the passed Generator for a value at the passed x / y position. 
+		/// Polls the passed Generator for a value at the passed x / z position. 
 		/// Applies spread and amplitude to computation.
 		/// </summary>
 		/// <param name="xPos">X position to get value at</param>
@@ -90,13 +90,29 @@ namespace Terra.Terrain {
 			return gen.GetValue(xPos * spread, zPos * spread, 0f) * settings.Amplitude;
 		}
 
-		/// <summary>
-		/// Updates this TerrainTiles position by taking a Vector2 where 
-		/// the x and y values are integers on a grid. Internally the x and y values 
-		/// are multiplied by the Length of the mesh specified in TerraSettings
+        /// <summary>
+		/// Polls the passed Generator for a value at the passed x / z position. 
+		/// Applies spread and amplitude from TerraSettings global instance to computation.
 		/// </summary>
-		/// <param name="position">Position to set the TerrainTile to (ie [1,0])</param>
-		public void UpdatePosition(Vector2 position) {
+		/// <param name="xPos">X position to get value at</param>
+		/// <param name="zPos">Z position to get value at</param>
+		/// <returns>height value if available, float default otherwise (0.0)</returns>
+		public static float PollGenerator(float xPos, float zPos) {
+            var sett = TerraSettings.Instance;
+            if (sett != null && sett.Graph.GetEndGenerator() != null) {
+                return PollGenerator(xPos, zPos, sett, sett.Graph.GetEndGenerator());
+            }
+
+            return default(float);
+        }
+
+        /// <summary>
+        /// Updates this TerrainTiles position by taking a Vector2 where 
+        /// the x and y values are integers on a grid. Internally the x and y values 
+        /// are multiplied by the Length of the mesh specified in TerraSettings
+        /// </summary>
+        /// <param name="position">Position to set the TerrainTile to (ie [1,0])</param>
+        public void UpdatePosition(Vector2 position) {
 			Position = position;
 			int len = Settings.Length;
 			transform.position = new Vector3(position.x * len, 0f, position.y * len);
@@ -185,18 +201,64 @@ namespace Terra.Terrain {
 			mr.sharedMaterial = mat;
 		}
 
-		/// <summary>
-		/// Creates a "raw" mesh from the passed generator and settings specified 
-		/// in <c>TerraSettings</c>. This method can be executed off the main thread.
-		/// 
-		/// Because of Unity's incompatibility with multithreading, a <c>MeshData</c> 
-		/// struct is returned with contents of the generated Mesh, instead of using the 
-		/// <c>Mesh</c> class.
-		/// </summary>
-		/// <param name="position">Position in tile grid, used for polling generator</param>
-		/// <param name="gen">Generator to apply</param>
-		/// <returns>triangles, vertices, normals, and UVs of the generated mesh</returns>
-		public MeshData CreateRawMesh(Vector2 position, Generator gen) {
+        /// <summary>
+        /// Creates a raw mesh from the length specified in TerraSettings and from the 
+        /// passed resolution. The passed vertices are directly passed into the MeshData 
+        /// structure. This method is useful for dynamically changing the resolution of 
+        /// the mesh at runtime (such as setting the LOD level).
+        /// </summary>
+        /// <param name="heightmap">vertices in world space to apply to the mesh</param>
+        /// <param name="res">resolution of the heightmap</param>
+        public MeshData CreateRawMesh(Vector3[] vertices, int res) {
+            float len = Settings.Length;
+            float spread = 1f / (Settings.Spread * Settings.MeshResolution);
+
+            Vector2[] uvs = new Vector2[vertices.Length];
+            for (int v = 0; v < res; v++) {
+                for (int u = 0; u < res; u++) {
+                    uvs[u + v * res] = new Vector2((float)u / (res - 1), (float)v / (res - 1));
+                }
+            }
+
+            int nbFaces = (res - 1) * (res - 1);
+            int[] triangles = new int[nbFaces * 6];
+            int t = 0;
+            for (int face = 0; face < nbFaces; face++) {
+                int i = face % (res - 1) + (face / (res - 1) * res);
+
+                triangles[t++] = i + res;
+                triangles[t++] = i + 1;
+                triangles[t++] = i;
+
+                triangles[t++] = i + res;
+                triangles[t++] = i + res + 1;
+                triangles[t++] = i + 1;
+            }
+
+            Vector3[] normals = new Vector3[vertices.Length];
+            CalculateNormalsManaged(vertices, normals, triangles);
+
+            MeshData mesh = new MeshData();
+            mesh.triangles = triangles;
+            mesh.vertices = vertices;
+            mesh.normals = normals;
+            mesh.uvs = uvs;
+
+            return mesh;
+        }
+
+        /// <summary>
+        /// Creates a "raw" mesh from the passed generator and settings specified 
+        /// in <c>TerraSettings</c>. This method can be executed off the main thread.
+        /// 
+        /// Because of Unity's incompatibility with multithreading, a <c>MeshData</c> 
+        /// struct is returned with contents of the generated Mesh, instead of using the 
+        /// <c>Mesh</c> class.
+        /// </summary>
+        /// <param name="position">Position in tile grid, used for polling generator</param>
+        /// <param name="gen">Generator to apply</param>
+        /// <returns>triangles, vertices, normals, and UVs of the generated mesh</returns>
+        public MeshData CreateRawMesh(Vector2 position, Generator gen) {
 			return CreateRawMesh(Settings, position, gen);
 		}
 
@@ -205,9 +267,9 @@ namespace Terra.Terrain {
 		/// Creates a "raw" mesh from the passed generator and settings specified 
 		/// in <c>TerraSettings</c>. This method can be executed off the main thread.
 		/// 
-		/// Because of Unity's incompatibility with multithreading, a <c>MeshData</c> 
+		/// Because of Unity's incompatibility with multithreading, a <see cref="MeshData"></see> 
 		/// struct is returned with contents of the generated Mesh, instead of using the 
-		/// <c>Mesh</c> class.
+		/// <see cref="Mesh"></see> class.
 		/// </summary>
 		/// <param name="position">Position in tile grid, used for polling generator</param>
 		/// <param name="gen">Generator to apply</param>
@@ -219,14 +281,8 @@ namespace Terra.Terrain {
 
 			Vector3[] vertices = new Vector3[res * res];
 			for (int z = 0; z < res; z++) {
-				float zPos = ((float)z / (res - 1) - .5f) * len;
-
 				for (int x = 0; x < res; x++) {
-					float xPos = ((float)x / (res - 1) - .5f) * len;
-					float yPos = gen.GetValue(((position.x * len) + xPos) * spread,
-						((position.y * len) + zPos) * spread, 0f) * settings.Amplitude;
-
-					vertices[x + z * res] = new Vector3(xPos, yPos, zPos);
+                    vertices[x + z * res] = GetPositionAt(x, z, res, settings, gen, position);
 				}
 			}
 
@@ -264,16 +320,53 @@ namespace Terra.Terrain {
 			return mesh;
 		}
 
+        /// <summary>
+        /// Polls the <see cref="Generator"/> found in <see cref="TerraSettings"/> and 
+        /// retrieves the y height value and x & z world coordinates for the passed x 
+        /// and z location. The x and z values are integers representing a local point on 
+        /// the to-be-created mesh. Internally, these values are transformed into world 
+        /// coordinates based on the passed resolution.
+        /// </summary>
+        /// <param name="xPos">mesh x position</param>
+        /// <param name="zPos">mesh z position</param>
+        /// <param name="resolution">resolution of this mesh</param>
+        /// <returns></returns>
+        public Vector3 GetPositionAt(int xPos, int zPos, int resolution) {
+            return GetPositionAt(xPos, zPos, resolution, Settings, Settings.Graph.GetEndGenerator(), Position);
+        }
+
+        /// <summary>
+        /// Static version of <see cref="GetPositionAt(int, int, int)"/>
+        /// </summary>
+        /// <returns></returns>
+        public static Vector3 GetPositionAt(int xPos, int zPos, int resolution, TerraSettings settings, Generator gen, Vector2 position) {
+            float amp = settings.Amplitude;
+            float spread = settings.Spread;
+            float length = settings.Length;
+
+            float worldX = ((float)xPos / (resolution - 1) - .5f) * length;
+            float worldZ = ((float)zPos / (resolution - 1) - .5f) * length;
+            float worldY = gen.GetValue(((position.x * length) + xPos) * spread,
+                ((position.y * length) + zPos) * spread, 0f) * settings.Amplitude;
+
+            return new Vector3(worldX, worldY, worldZ);
+        }
+
 		/// <summary>
 		/// Renders the passed MeshData as a Mesh in the scene 
-		/// by creating a MeshRenderer and MeshFilter.
+		/// by creating a MeshRenderer and MeshFilter if they do 
+        /// not exist already.
 		/// </summary>
 		/// <param name="data">Mesh information to apply</param>
 		public void RenderRawMeshData(MeshData data) {
-			MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
-			renderer.material = new Material(Shader.Find("Diffuse"));
-			Terrain = gameObject.AddComponent<MeshFilter>().mesh;
-			
+            if (gameObject.GetComponent<MeshRenderer>() == null) {
+                MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
+                renderer.material = new Material(Shader.Find("Diffuse"));
+            }
+            if (gameObject.GetComponent<MeshFilter>() == null) {
+                Terrain = gameObject.AddComponent<MeshFilter>().mesh;
+            }
+
 			Terrain.vertices = data.vertices;
 			Terrain.triangles = data.triangles;
 			Terrain.uv = data.uvs;
